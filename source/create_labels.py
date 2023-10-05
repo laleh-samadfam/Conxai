0f343db7d958f0c19d00bb40425bf65c8711546c
laleh-samadfam <laleh.samadfam@gmail.com>
2023-10-05 20:24:55 +0200

    labels are padded to  max length

--- source/create_labels.py
+++ source/create_labels.py
@@ -2,24 +2,30 @@ import pandas as pd
 import os
 import pandas as pd
 
-def label_number(stages):
+def label_number(stages, max_length):
 
     stage_names = stages.split()
-    stage_numbers = [stage[6] for stage in stage_names if len(stage) >= 6]
-    result_string = ' '.join(stage_numbers)
-    return result_string
+    stage_numbers = [int(stage[6]) for stage in stage_names if len(stage) >= 6]
+
+    stage_numbers = sorted(stage_numbers)
+
+    stage_numbers.extend([-1] * (max_length - len(stage_numbers)))
+
+    return stage_numbers
 
 
 label_path = '../data/processed_data/clean_labels.csv'
 df = pd.read_csv(label_path)
 
 grouped = df.groupby('cj')['label'].apply(lambda x: ' '.join(x)).reset_index()
+max_label_length = max(len(row['label'].split()) for _, row in grouped.iterrows())
 
 # Iterate over groups and create text files
 for _, row in grouped.iterrows():
     cj_value = row['cj']
     labels_text = row['label']
-    print(label_number(labels_text))
+    label_numbers = label_number(labels_text, max_label_length)
+    my_str_list = [str(num) for num in label_numbers]
 
     # Create a directory if it doesn't exist
     os.makedirs('train_labels', exist_ok=True)  # Adjust 'output_folder' as needed
@@ -27,6 +33,6 @@ for _, row in grouped.iterrows():
     # Write the labels to a text file
     file_path = os.path.join('train_labels', f'{cj_value}.txt')
     with open(file_path, 'w') as f:
-        f.write(label_number(labels_text))
+        f.write(' '.join(my_str_list))
 
     print(f'Text file created for {cj_value} at: {file_path}')
